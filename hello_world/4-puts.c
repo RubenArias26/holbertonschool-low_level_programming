2b1f0ded82eb8cd7c6c0a4ec403870729909c1cb
RubenArias26 <ruben.arias.5.26@gmail.com>
2023-08-02 20:23:38 -0700

    I added a c program that prints a sentence

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*
+ * File : 4-outs.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints programing is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+    return (0);
+}
