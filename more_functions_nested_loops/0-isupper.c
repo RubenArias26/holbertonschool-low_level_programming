6f153fafcd6e25057b394ca47c64184f605294ce
RubenArias26 <ruben.arias.5.26@gmail.com>
2023-08-20 18:02:17 -0700

    I added a new directory

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/*
+ * File: 0-isupper.c
+ */
+
+#include "school.h"
+
+/**
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
+}
